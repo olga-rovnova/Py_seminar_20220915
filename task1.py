1aa6122d429d86057a10a94b1b008df1469a0008
Olga <olga-rovnova@yandex.ru>
2022-09-18 23:10:43 +0300

    task1

--- task1.py
+++ task1.py
@@ -1,7 +1,16 @@
-# 1.Напишите программу, удаляющую из текста все слова, содержащие "абв"
+# 1.Напишите программу, удаляющую из текста все слова, содержащие "абв". 
 
-tex = 'Напишитеабв программуабвабв, удаляющуюабв из текста все слова, содержащие "абв"'
-lst = list(tex.split()) 
+# f = open('task1.txt', 'a')
+# f.writelines('writeabc a program deleting all words with "abc"')
+# f.close
+
+f = open('task1.txt', 'rt')
+line = f.readlines()
+f.close
+print(line)
+
+line1 = str(line)
+lst = list(line1.split()) 
 print(lst) 
-lst = list(filter(lambda x: 'абв' not in x, lst)) 
+lst = list(filter(lambda x: 'abc' not in x, lst)) 
 print(' '.join(lst))
